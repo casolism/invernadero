b55f60d4219b9b8f9a531c1b7312df6af3120b80
Carlos Solis <ca_solis@hotmail.com>
2013-12-11 17:04:53 -0600

    Se modificó bug en el método de agregar usuario

--- Aplicaciones/MISEC.TemperaturaServices/MISEC.ISW.Temperatura.Servicios/UsuariosServices.asmx.cs
+++ Aplicaciones/MISEC.TemperaturaServices/MISEC.ISW.Temperatura.Servicios/UsuariosServices.asmx.cs
@@ -36,7 +36,7 @@ namespace MISEC.ISW.Temperatura.Servicios
         }
 
         [WebMethod(Description = "Inserta usuario en la BD")]
-        public int InsertaUsuario(string IdUsuario, string Password,int Nivel)
+        public void InsertaUsuario(string IdUsuario, string Password,int Nivel)
         {
             dbControlManager db = new dbControlManager();
             var idInsertado = db.Usuario.InsertWithIdentity(() => new dbControl.Usuario
@@ -45,7 +45,6 @@ namespace MISEC.ISW.Temperatura.Servicios
                 Password = Password,
                 Nivel=Nivel
             });
-            return Convert.ToInt32(idInsertado);
         }
         
         [WebMethod(Description = "Actualiza usuario en la BD")]
